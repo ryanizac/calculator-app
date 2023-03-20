735ca960b90dee2cf02b30eda1695cc3b00ba455
Ryan Izac <ryanizac.infor@gmail.com>
2023-03-19 23:10:52 -0300

    feat: update result when key is pressed

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,13 +3,17 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 
 export function App() {
-  const [result] = useState("");
+  const [result, setResult] = useState("");
+
+  function onPressKeys(key: string) {
+    setResult((prev) => prev + key);
+  }
 
   return (
     <Template>
       <StatusBar style="light" />
       <ResultArea result={result} />
-      <Keyboard />
+      <Keyboard onPress={onPressKeys} />
     </Template>
   );
 }
