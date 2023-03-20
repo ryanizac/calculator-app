8a9e96bdca7cd3c406f904688a62c2bf7bd9b228
Ryan Izac <ryanizac.infor@gmail.com>
2023-03-19 23:25:21 -0300

    feat: implement clear function

--- src/core/calculator-core.ts
+++ src/core/calculator-core.ts
@@ -1,7 +1,15 @@
 import { Keys } from "./keys";
 
 export class CalculatorCore {
+  static clear() {
+    return "0";
+  }
+
   static Resolve(currentValue: string, key: Keys): string {
+    if (key === "AC") {
+      return this.clear();
+    }
+
     return currentValue + key;
   }
 }
