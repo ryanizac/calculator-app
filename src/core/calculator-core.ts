10f3d4c720071e43e9a9527f4fdc3b15d68ceea2
Ryan Izac <ryanizac.infor@gmail.com>
2023-03-19 23:27:05 -0300

    feat: implement backspace function

--- src/core/calculator-core.ts
+++ src/core/calculator-core.ts
@@ -5,9 +5,23 @@ export class CalculatorCore {
     return "0";
   }
 
+  static backspace(currentValue: string) {
+    const newText = currentValue.slice(0, currentValue.length - 1);
+
+    if (newText === "") {
+      return "0";
+    }
+
+    return newText;
+  }
+
   static Resolve(currentValue: string, key: Keys): string {
     if (key === "AC") {
       return this.clear();
+    }
+
+    if (key === "<-") {
+      return this.backspace(currentValue);
     }
 
     return currentValue + key;
