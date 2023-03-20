5dbe2f1fe21e759b48ef00cbc2326decdb8f17f3
Ryan Izac <ryanizac.infor@gmail.com>
2023-03-19 23:09:26 -0300

    feat: add calculator keys to keyboard

--- src/components/keyboard/index.tsx
+++ src/components/keyboard/index.tsx
@@ -5,29 +5,31 @@ import { styles } from "./styles";
 export function Keyboard(props: Keyboard.Props) {
   return (
     <View style={styles.container}>
-      <Key name="AC" />
-      <Key name="<-" />
-      <Key name="%" />
-      <Key name="/" />
-      <Key name="7" />
-      <Key name="8" />
-      <Key name="9" />
-      <Key name="*" />
-      <Key name="4" />
-      <Key name="5" />
-      <Key name="6" />
-      <Key name="-" />
-      <Key name="1" />
-      <Key name="2" />
-      <Key name="3" />
-      <Key name="+" />
-      <Key name="0" large />
-      <Key name="," />
-      <Key name="=" orange />
+      <Key name="AC" onPress={props.onPress} />
+      <Key name="<-" onPress={props.onPress} />
+      <Key name="%" onPress={props.onPress} />
+      <Key name="/" onPress={props.onPress} />
+      <Key name="7" onPress={props.onPress} />
+      <Key name="8" onPress={props.onPress} />
+      <Key name="9" onPress={props.onPress} />
+      <Key name="*" onPress={props.onPress} />
+      <Key name="4" onPress={props.onPress} />
+      <Key name="5" onPress={props.onPress} />
+      <Key name="6" onPress={props.onPress} />
+      <Key name="-" onPress={props.onPress} />
+      <Key name="1" onPress={props.onPress} />
+      <Key name="2" onPress={props.onPress} />
+      <Key name="3" onPress={props.onPress} />
+      <Key name="+" onPress={props.onPress} />
+      <Key name="0" large onPress={props.onPress} />
+      <Key name="," onPress={props.onPress} />
+      <Key name="=" orange onPress={props.onPress} />
     </View>
   );
 }
 
 export namespace Keyboard {
-  export type Props = {};
+  export type Props = {
+    onPress?: (key: string) => void;
+  };
 }
